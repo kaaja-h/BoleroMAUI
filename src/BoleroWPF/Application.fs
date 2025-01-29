13abc0d6e00d325a7bb745b65f378fd345b92d7f
Karel Honzl <karel.honzl@gmail.com>
2025-01-29 14:55:01 +0100

    WPF transfered to f#

---
+++ src/BoleroWPF/Application.fs
@@ -0,0 +1,31 @@
+﻿namespace BoleroWPFApplication
+
+open System.Windows;
+open System.Windows.Controls;
+open System.Windows.Media;
+open System.Windows.Ink
+open Microsoft.AspNetCore.Components.WebView.Wpf
+
+
+type MainApp<'T>()=
+    inherit Application()
+    
+    let root = new Window()
+    let blazor =     BlazorWebView()
+        
+    member this.Init(services )=
+        root.Show()
+        root.ResizeMode <- ResizeMode.CanResizeWithGrip
+        let grid = Grid()
+        blazor.HostPage <- "wwwroot\index.html"
+        blazor.Services <- services
+        let rootComponent = RootComponent()
+        rootComponent.Selector<-"#app"
+        rootComponent.ComponentType <- typeof<'T>
+        blazor.RootComponents.Add(rootComponent)
+        grid.Children.Add(blazor) |> ignore
+        
+        root.Content <- grid       
+        base.MainWindow <- root
+        base.MainWindow.Closed.Add( fun x -> this.Shutdown(0))
+        ()
---
+++ src/BoleroWPF/Program.fs
@@ -0,0 +1,18 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+module BoleroWPF.Program
+
+open System
+open Bolero.Remoting.Client
+open BoleroMAUI.Shared
+open BoleroWPFApplication
+open Microsoft.Extensions.DependencyInjection
+[<EntryPoint;STAThread>]
+let main args =
+    let app = new MainApp<MyApp>()
+    let serviceCollection = new ServiceCollection()
+    serviceCollection.AddWpfBlazorWebView()|> ignore
+    serviceCollection.AddBoleroRemoting(fun a -> a.BaseAddress <- new Uri("http://localhost:5005/") )|> ignore
+
+    app.Init(serviceCollection.BuildServiceProvider()) |> ignore
+    app.Run()
+    
